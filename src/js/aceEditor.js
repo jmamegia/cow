32fe5dbd47ca75a456bcff9b7dc099297e7c50f9
Juanma Megia <jma.megia@gmail.com>
2021-07-31 10:05:02 +0200

    font size zoom added

--- src/js/aceEditor.js
+++ src/js/aceEditor.js
@@ -1,13 +1,16 @@
 const htmlEditor = ace.edit("htmlForm");
 htmlEditor.setTheme("ace/theme/monokai");
 htmlEditor.getSession().setMode("ace/mode/html");
+htmlEditor.setFontSize(15);
 
 const cssEditor = ace.edit("cssForm");
 cssEditor.setTheme("ace/theme/kr_theme");
 cssEditor.getSession().setMode("ace/mode/css");
+cssEditor.setFontSize(15);
 
 const jsEditor = ace.edit("jsForm");
 jsEditor.setTheme("ace/theme/dracula");
 jsEditor.getSession().setMode("ace/mode/javascript");
+jsEditor.setFontSize(15);
 
-export  {htmlEditor, cssEditor, jsEditor}
+export { htmlEditor, cssEditor, jsEditor };
--- src/js/actionIcons.js
+++ src/js/actionIcons.js
@@ -2,6 +2,7 @@ import maximice from "../assets/img/four-arrows-interface-symbol-to-maximize-siz
 import minimize from "../assets/img/minimize-arrows.svg";
 import show from "../assets/img/visible-eye.svg";
 import hide from "../assets/img/close-eyes.svg";
+import { htmlEditor, cssEditor, jsEditor } from "./aceEditor";
 
 const resize = (evt) => {
   const targetName = evt.target.getAttribute("target");
@@ -23,12 +24,50 @@ const toggleVisibility = () => {
   else button.setAttribute("src", show);
 };
 
-export default function addActionIcons(area) {
-  const actionButonPanels = document.querySelectorAll(
-    ".action-buttons .resize"
-  );
-  actionButonPanels.forEach((el) => el.addEventListener("click", resize));
+const plusZoom = (evt) => {
+  const targetName = evt.target.getAttribute("target");
+  switch (targetName) {
+    case "#htmlForm":
+      htmlEditor.setFontSize(htmlEditor.getFontSize() + 1);
+      break;
+    case "#cssForm":
+      cssEditor.setFontSize(cssEditor.getFontSize() + 1);
+      break;
+    case "#jsForm":
+      jsEditor.setFontSize(jsEditor.getFontSize() + 1);
+      break;
+  }
+};
 
-  const toggleActionIconButton = document.querySelector("#toggleActionIcon");
-  toggleActionIconButton.addEventListener("click", toggleVisibility);
+const minusZoom = (evt) => {
+  const targetName = evt.target.getAttribute("target");
+  switch (targetName) {
+    case "#htmlForm":
+      htmlEditor.setFontSize(htmlEditor.getFontSize() - 1);
+      break;
+    case "#cssForm":
+      cssEditor.setFontSize(cssEditor.getFontSize() - 1);
+      break;
+    case "#jsForm":
+      jsEditor.setFontSize(jsEditor.getFontSize() - 1);
+      break;
+  }
+};
+
+export default function addActionIcons(area) {
+  const resizeIcons = document.querySelectorAll(".action-buttons .resize");
+  resizeIcons.forEach((el) => el.addEventListener("click", resize));
+
+  const toggleVisibilityIcon = document.querySelector("#toggleActionIcon");
+  toggleVisibilityIcon.addEventListener("click", toggleVisibility);
+
+  const plusZoomIcons = document.querySelectorAll(
+    ".action-buttons .font-size-plus"
+  );
+  plusZoomIcons.forEach((el) => el.addEventListener("click", plusZoom));
+
+  const minusZoomIcons = document.querySelectorAll(
+    ".action-buttons .font-size-minus"
+  );
+  minusZoomIcons.forEach((el) => el.addEventListener("click", minusZoom));
 }
